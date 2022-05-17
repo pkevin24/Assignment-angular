0f1392f6e33e5927e1c29452096e85c494203262
pkevin24 <70275725+pkevin24@users.noreply.github.com>
2022-05-17 20:34:27 +0530

    .

--- web1/src/app/about/about.component.ts
+++ web1/src/app/about/about.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
-
+export class AboutComponent {
+  data=[{
+    title:"Kevin",
+    content:"Hey this is Kevin"
+  },{
+    title:"Prince",
+    content:"Hey this is Prince"
+  },{
+    title:"Simi",
+    content:"Hey this is Simi"
+  }]
+  
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
